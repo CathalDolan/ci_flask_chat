2c0491d1d119e2487ee20a2fd70c791fb966c756
CathalDolan <cathal@gizagig.com>
2020-09-14 14:12:41 +0000

    Push to Github

--- run.py
+++ run.py
@@ -65,5 +65,7 @@ def send_message(username, message):
 app.run(
     host=os.environ.get("IP", "0.0.0.0"),
     port=int(os.environ.get("PORT", "5000")),
-    debug=True
+    debug=False
     )
+
+    
