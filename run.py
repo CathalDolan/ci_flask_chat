cc7cbe9978da504513e01de650304e9f894b53f0
CathalDolan <cathal@gizagig.com>
2020-09-14 09:54:39 +0000

    Added chat textbox functionality and refactored code

--- run.py
+++ run.py
@@ -1,38 +1,63 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session, url_for
+
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 messages = []
 
 
-def add_messages(username, message):
+def add_message(username, message):
     """Add messages to the `messages` list"""
-    messages.append("{}: {}".format(username, message))
+    now = datetime.now().strftime("%H:%M:%S")
+    # messages.append("({}) {}: {}".format(now, username, message))  Used before dict created below
+    # messages_dict = {"timestamp": now, "from": username, "message": message}
+    messages.append({"timestamp": now, "from": username, "message": message})
 
-
+""" # This was removed when dict added above. Also, call from user() fn removed from return .format
 def get_all_messages():
-    """Get all messages and separate them with a 'br' tag"""
+    # Get all messages and separate them with a 'br' tag
     return "<br>".join(messages)
+"""
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
     """Main page with welcome & instructions"""
-    return "<h1>Hello World!</h1><p>To send a message: /USERNAME/MESSAGE</p>"
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        # return redirect(session["username"])  Replaced by below. Also user() fn
+        return redirect(url_for("user", username=session["username"]))
+
+    # return "<h1>Hello World!</h1><p>To send a message: /USERNAME/MESSAGE</p>" Before render_template was added
+    return render_template("index.html")
 
 
-@app.route('/<username>')
+@app.route('/chat/<username>', methods=["GET", "POST"])
 def user(username):
-    """Display Chat messages"""
-    return "<h1>Welcome, {0}</h1> {1}".format(username, get_all_messages())
+    """Add and Display Chat messages"""
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_message(username, message)
+        # return redirect(session["username"])  Replaced by below. Also index() fn
+        return redirect(url_for("user", username=session["username"]))
+
+    # return "<h1>Welcome, {0}</h1> {1}".format(username, messages) otrequired once chat.html introduced
+    return render_template("chat.html", username = username, chat_messages = messages)
 
 
+""" Not required because messages are added via textbox
 @app.route('/<username>/<message>')
 def send_message(username, message):
-    """Create a new message and redirect back to the chat page"""
+    #Create a new message and redirect back to the chat page
     #return "{0}: {1}".format(username, message)
-    add_messages(username, message)
+    add_message(username, message)
     return redirect("/" + username)  #returns the user to their personalised page
+"""
 
 
 app.run(
